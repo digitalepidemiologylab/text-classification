b083253eea602d7574f68d5b1c94064f0f328632
Martin Müller <martin.mathias.mueller@gmail.com>
2020-02-25 22:05:05 +0100

    improve confusion matrix plot

--- utils/plot_helpers.py
+++ utils/plot_helpers.py
@@ -1,4 +1,5 @@
 import os
+import joblib
 import logging
 import sklearn.metrics
 import pandas as pd
@@ -17,11 +18,23 @@ def plot_confusion_matrix(run):
     if not os.path.isfile(test_output_file):
         raise FileNotFoundError(f'No file {test_output_file} found for run {run}. Pass the option `write_test_output: true` when training the model.')
     df = pd.read_csv(test_output_file)
+    label_mapping = get_label_mapping(f_path)
+    labels = list(label_mapping.keys())
     cnf_matrix = sklearn.metrics.confusion_matrix(df.label, df.prediction)
-    # plot
-    fig, ax = plt.subplots(1, 1, figsize=(6,6))
-    sns.heatmap(cnf_matrix, ax=ax, annot=True)
+    df = pd.DataFrame(cnf_matrix, columns=labels, index=labels)
+    # plotting
+    fig, ax = plt.subplots(1, 1, figsize=(6,4))
+    sns.heatmap(df, ax=ax, annot=True, fmt='d', annot_kws={"fontsize":8})
+    ax.set(xlabel='predicted label', ylabel='true label')
     save_fig(fig, f_path, 'confusion_matrix')
+
+def get_label_mapping(run_path):
+    label_mapping_path = os.path.join(run_path, 'label_mapping.pkl')
+    if not os.path.isfile(label_mapping_path):
+        raise FileNotFoundError(f'Could not find label mapping file {label_mapping_path}')
+    with open(label_mapping_path, 'rb') as f:
+        label_mapping = joblib.load(f)
+    return label_mapping
 
 def save_fig(fig, folder_path, name, plot_formats=['png'], dpi=300):
     def f_name(fmt):
