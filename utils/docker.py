3c33eb3add8bebc72da2065efc969cea53a89fe3
Martin Müller <martin.mathias.mueller@gmail.com>
2020-03-10 17:47:35 +0100

    deploy using sagemaker

---
+++ utils/docker.py
@@ -0,0 +1,126 @@
+import os
+import docker
+import logging
+from docker.errors import APIError, BuildError, ImageNotFound
+from utils.helpers import find_project_root
+from utils.ecr import ECR
+
+logger = logging.getLogger(__name__)
+
+class Docker():
+    def __init__(self):
+        self.docker_client = None
+        self.docker_api_client = None
+        self.ecr = ECR()
+
+    @property
+    def client(self):
+        if self.docker_client is None:
+            self.docker_client = docker.from_env()
+        return self.docker_client
+
+    @property
+    def api_client(self):
+        if self.docker_api_client is None:
+            self.docker_api_client =  docker.APIClient()
+        return self.docker_api_client
+
+    def get_image(self, image_name):
+        try:
+            image = self.client.images.get(image_name)
+        except ImageNotFound as e:
+            logger.error(f'Could not find image {image_name}')
+            raise e
+        return image
+
+    def build(self, docker_path, image_name):
+        # build local image
+        labels = {'platform': 'crowdbreaks'}
+        logger.info(f'Building Docker image from {docker_path}...')
+        try:
+            logs = self.api_client.build(path=docker_path, tag=image_name, rm=True, labels=labels, decode=True)
+            self._get_stream_log(logs)
+        except (APIError, BuildError, TypeError) as e:
+            logger.error('Build unsucessful. Build log:')
+            self._get_stream_log(logs)
+            raise e
+
+    def run(self, image_name, run, model_type):
+        """Serves model locally"""
+        model_path = os.path.join(find_project_root(), 'output', run)
+        docker_path = os.path.join(find_project_root(), 'sagemaker', model_type, 'src')
+        # bind model artefacts to /opt/ml and bind a volume for the code
+        volumes = {model_path: {'bind': '/opt/ml/model', 'mode': 'ro'}, docker_path: {'bind': '/opt/program'}}
+        ports = {'5000':'5000'}
+        command = 'python3 serve'
+        environment = {'FLASK_ENV': 'DEVELOPMENT'}
+        try:
+            container = self.client.containers.run(image_name, command, volumes=volumes, ports=ports, detach=True, environment=environment)
+            logs = container.logs(stream=True)
+            self._get_stream_log(logs, decode=True)
+        except KeyboardInterrupt:
+            logger.info(f'Gracefully stopping docker container {image_name}...')
+            container.stop()
+
+    def list_tags(self):
+        resp = self.client.images.list(filters={'label': 'platform=crowdbreaks'})
+        tags = []
+        for r in resp:
+            tags.extend(r.attrs['RepoTags'])
+        return tags
+
+    def image_exists(self, image_name):
+        try:
+            self.client.images.get(image_name)
+        except:
+            return False
+        else:
+            return True
+
+    def get_ecr_auth_config(self):
+        token = self.ecr.get_auth_token()
+        username, password = token.split(':')
+        return {'username': username, 'password': password}
+
+    def push(self, image_name):
+        # check if local image exists
+        if not self.image_exists(image_name):
+            logger.error(f'Image {image_name} does not exist locally. You need to build it first!')
+            return
+        # login to AWS ECR
+        logger.info('Attempting to log in to AWS ECR...')
+        auth_config = self.get_ecr_auth_config()
+        # create repository if it doesn't exist yet
+        self.ecr.create_repository(image_name)
+        # tag existing image with ECR name
+        ecr_name = self.ecr.get_ecr_image_name(image_name)
+        logger.info(f'Tagging image {image_name} as {ecr_name}...')
+        image = self.get_image(image_name)
+        image.tag(ecr_name)
+        logger.info('Pushing tag to AWS ECR...')
+        try:
+            logs = self.api_client.push(ecr_name, stream=True, decode=True, auth_config=auth_config)
+            self._get_stream_log(logs)
+        except APIError as e:
+            logger.error('Pushing image to ECR unsucessful.')
+            self._get_stream_log(logs)
+            raise e
+        else:
+            logger.info('Successfully pushed image to ECR.')
+
+
+    # private
+
+    def _get_stream_log(self, stream_log, decode=False):
+        for log in stream_log:
+            if decode:
+                log = log.decode().strip()
+            if 'stream' in log:
+                out_log = log['stream'].strip()
+                if len(out_log) > 0:
+                    logger.info(out_log)
+            elif 'errorDetail' in log:
+                raise Exception(log['errorDetail'])
+            else:
+                logger.info(log)
+
---
+++ utils/s3.py
@@ -0,0 +1,53 @@
+import os
+import boto3
+import logging
+import tarfile
+from utils.helpers import find_project_root
+
+
+logger = logging.getLogger(__name__)
+
+class S3():
+    def __init__(self, bucket_name='crowdbreaks-sagemaker'):
+        self.bucket_name = bucket_name
+        self.s3_client = None
+        self.tar_name = 'model.tar.gz'
+
+    @property
+    def client(self):
+        if self.s3_client is None:
+            self.s3_client = boto3.client('s3')
+        return self.s3_client
+
+    def upload_model(self, run, image_name,  model_type='fasttext'):
+        run_dir = os.path.join(find_project_root(), 'output', run)
+        if not os.path.isdir(run_dir):
+            raise FileNotFoundError(f'Could not find run directory {run_dir}')
+        # compile model artefacts
+        default_model_files = ['label_mapping.pkl', 'run_config.json']
+        if model_type == 'fasttext':
+            model_files = ['model.bin', 'model_config.json']
+        else:
+            raise ValueError(f'Model type {model_type} is not yet supported')
+        model_files += default_model_files
+        input_files = [os.path.join(run_dir, model_file) for model_file in model_files]
+        # tar
+        model_tarfile = os.path.join(run_dir, self.tar_name)
+        self.make_tar(input_files, model_tarfile)
+        # upload
+        key = self.get_model_s3_key(image_name)
+        logger.info(f'Uploading {model_tarfile} to S3 bucket {self.bucket_name} under key {key}...')
+        self.client.upload_file(model_tarfile, self.bucket_name, key)
+
+    def get_model_s3_key(self, image_name):
+        return os.path.join('output', image_name, self.tar_name)
+
+    def get_model_s3_path(self, image_name):
+        key = self.get_model_s3_key(image_name)
+        return f's3://{self.bucket_name}/{key}'
+
+    def make_tar(self, input_files, output_file):
+        with tarfile.open(output_file, "w:gz") as tar:
+            for input_file in input_files:
+                logger.info(f'Adding {input_file} to tarfile {output_file}...')
+                tar.add(input_file, arcname=os.path.basename(input_file))
